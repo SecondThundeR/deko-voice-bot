b41252d95e6c75443a29e1e0759509ab1936e794
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-22 18:39:06 +0300

    chore(deps): export `User` and `Collection` types

--- deps.ts
+++ deps.ts
@@ -7,7 +7,12 @@ export {
   HttpError,
   Api,
 } from "grammy/mod.ts";
-export type { InlineQueryResultVoice, BotCommand } from "grammy/types.ts";
+export type { InlineQueryResultVoice, BotCommand, User } from "grammy/types.ts";
 export { run, type RunnerHandle, sequentialize } from "grammy_runner/mod.ts";
 export { load as dotenv } from "dotenv/mod.ts";
-export { Bson, MongoClient, type ObjectId } from "mongo/mod.ts";
+export {
+  Bson,
+  MongoClient,
+  type ObjectId,
+  type Collection,
+} from "mongo/mod.ts";
