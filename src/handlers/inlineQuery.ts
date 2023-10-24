47d1d0ba5c08dc31809279b0e55edb05a8bfcea2
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-24 21:38:59 +0300

    feat(handlers): add support for `maintenance` mode

--- src/handlers/inlineQuery.ts
+++ src/handlers/inlineQuery.ts
@@ -1,6 +1,8 @@
 import { Composer } from "@/deps.ts";
 
+import { featureFlags } from "@/src/constants/database.ts";
 import { maxQueryElementsPerPage } from "@/src/constants/inline.ts";
+import { locale } from "@/src/constants/locale.ts";
 import {
     updateUsersStats,
     updateVoiceStats,
@@ -8,6 +10,9 @@ import {
 import { offsetArray } from "@/src/helpers/array.ts";
 import { getCurrentButtonText } from "@/src/helpers/inlineQuery.ts";
 import { getCurrentVoiceQueriesData } from "@/src/helpers/voices.ts";
+import { featureFlagsCache } from "@/src/cache/featureFlags.ts";
+
+const { button } = locale.frontend.maintenance;
 
 const inlineQueryHandler = new Composer();
 
@@ -17,6 +22,16 @@ inlineQueryHandler.on("chosen_inline_result", async (ctx) => {
 });
 
 inlineQueryHandler.on("inline_query", async (ctx) => {
+    if (featureFlagsCache.get(featureFlags.maintenance)) {
+        await ctx.answerInlineQuery([], {
+            button: {
+                text: button,
+                start_parameter: featureFlags.maintenance,
+            },
+            cache_time: 0,
+        });
+    }
+
     const currentOffset = Number(ctx.update.inline_query.offset) || 0;
     const data = ctx.update.inline_query.query;
     const currentQueriesArray = await getCurrentVoiceQueriesData(data);
