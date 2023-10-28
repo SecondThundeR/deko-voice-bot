fd9e2b52330ba321ae1571809007e64d1e047612
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-28 13:41:20 +0300

    feat(constants): add locale for `mydata` command

--- src/constants/locale.ts
+++ src/constants/locale.ts
@@ -1,3 +1,5 @@
+import { extractUserDetails } from "@/src/helpers/api.ts";
+
 export const locale = {
     general: {
         googleExportLinkFail: "Failed to extract ID from provided link",
@@ -9,6 +11,18 @@ export const locale = {
         startHelp: (botUsername: string) =>
             `Чтобы отправлять фразы дЕко (или декО?) или найти ту самую нужную, введи в любом другом чате мою юзерку @${botUsername} или нажми кнопку снизу`,
         allVoices: "Отображение всех фраз (Для поиска начните вводить текст)",
+        failedToFindUserData:
+            "Не удалось получить данные! Похоже, вы использовали команду таким образом, что я не могу получить ваш ID",
+        userDataMessage: (
+            { userID, fullName, username }: NonNullable<
+                ReturnType<typeof extractUserDetails>
+            >,
+            usageAmount: number,
+        ) => `Для сохранения полной прозрачности, вот вся информация, которая хранится о Вас:\n${
+            userID && `- Ваш Telegram ID: <code>${userID}</code>`
+        }\n${fullName && `- Ваше полное имя в Telegram: ${fullName}`}\n${
+            username && `- Ваше имя пользователя в Telegram: @${username}`
+        }\n- Ваше количество отправленных реплик: ${usageAmount}\n\nЭти данные используются для построения приватных графиков использования, которые помогают увидеть охват бота в динамике и отобразить список самых активных пользователей\nЕсли вы хотите удалить эти данные из базы данных и больше не собирать хранить эти данные, используйте команду /optout`,
         filteredVoices: (str: string) => `Поиск: ${str}`,
         invalidatedSuccessfully: "Главный кеш был успешно инвалидирован!",
         maintenance: {
