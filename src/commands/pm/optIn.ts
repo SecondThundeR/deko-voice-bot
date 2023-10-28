28f0418ea0b3cc2ff09e0ff21ebe421662fb83b1
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-29 02:52:51 +0300

    feat(commands): update `optIn` command
    Add check if condition met (no data, no ignore ID) to send exception message with start keyboard

--- src/commands/pm/optIn.ts
+++ src/commands/pm/optIn.ts
@@ -1,11 +1,13 @@
 import { Composer } from "@/deps.ts";
 
 import { locale } from "@/src/constants/locale.ts";
+import { sendInlineRequestKeyboard } from "@/src/constants/keyboards.ts";
 import { removeIgnoredUser } from "@/src/database/deko/ignoredUsers/removeIgnoredUser.ts";
+import { isUserUsageExists } from "@/src/database/deko/stats/isUserUsageExists.ts";
 
 const optInCommand = new Composer();
 
-const { failedToFindUserData, removeIgnore: { success, failed } } =
+const { failedToFindUserData, removeIgnore: { success, failed, exception } } =
     locale.frontend;
 
 optInCommand.command("optin", async (ctx) => {
@@ -13,6 +15,13 @@ optInCommand.command("optin", async (ctx) => {
     if (!currentUserID) return await ctx.reply(failedToFindUserData);
 
     const removeIgnoreStatus = await removeIgnoredUser(currentUserID);
+    const isUserDataExists = await isUserUsageExists(currentUserID);
+    if (!isUserDataExists && !removeIgnoreStatus) {
+        return await ctx.reply(exception, {
+            reply_markup: sendInlineRequestKeyboard,
+        });
+    }
+
     return await ctx.reply(removeIgnoreStatus ? success : failed);
 });
 
