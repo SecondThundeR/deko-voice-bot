5c49464b74f4a04208623dac7ce397c6453fb1c9
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-06 18:25:36 +0300

    chore: add `grammy_menu` plugin and bump `dotenv`

--- deps.ts
+++ deps.ts
@@ -9,6 +9,7 @@ export {
 } from "grammy/mod.ts";
 export type { BotCommand, InlineQueryResultVoice, User } from "grammy/types.ts";
 export { run, type RunnerHandle, sequentialize } from "grammy_runner/mod.ts";
+export { Menu } from "grammy_menu/mod.ts";
 export { load as dotenv } from "dotenv/mod.ts";
 export {
     Bson,
