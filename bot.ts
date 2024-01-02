74d880476bc8eeb75956b58f311ad913c6071abe
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-01-03 02:39:41 +0300

    refactor: update bot entry point
    Add usage of `Deno.addSignalListener`
    Update callback for runner stop
    Reorder imports and some handler usages
    Move getting `botInfo` call after runner activation
    Remove usage of `node:process`

--- bot.ts
+++ bot.ts
@@ -1,6 +1,3 @@
-// @deno-types="npm:@types/node"
-import process from "node:process";
-
 import {
     Bot,
     dotenv,
@@ -12,21 +9,25 @@ import {
     session,
 } from "@/deps.ts";
 
-import { startCommand } from "@/src/commands/pm/start.ts";
+import { invalidateCommand } from "@/src/commands/pm/creator/invalidate.ts";
+import { maintenanceCommand } from "@/src/commands/pm/creator/maintenance.ts";
+import { favoritesCommand } from "@/src/commands/pm/favorites.ts";
 import { myDataCommand } from "@/src/commands/pm/myData.ts";
 import { optInCommand } from "@/src/commands/pm/optIn.ts";
 import { optOutCommand } from "@/src/commands/pm/optOut.ts";
-import { favoritesCommand } from "@/src/commands/pm/favorites.ts";
-import { invalidateCommand } from "@/src/commands/pm/creator/invalidate.ts";
-import { maintenanceCommand } from "@/src/commands/pm/creator/maintenance.ts";
+import { startCommand } from "@/src/commands/pm/start.ts";
+
 import { locale } from "@/src/constants/locale.ts";
-import { inlineQueryHandler } from "@/src/handlers/inlineQuery.ts";
+
+import { favoritesMenu } from "@/src/menu/favorites.ts";
+
 import {
     registerCreatorCommands,
     registerUserCommands,
 } from "@/src/helpers/api.ts";
-import { favoritesMenu } from "@/src/menu/favorites.ts";
-import { BotContext } from "@/src/types/bot.ts";
+import { inlineQueryHandler } from "@/src/handlers/inlineQuery.ts";
+
+import type { BotContext } from "@/src/types/bot.ts";
 
 await dotenv({ export: true });
 
@@ -35,6 +36,7 @@ const { envsCheckFail } = locale.general;
 const token = Deno.env.get("BOT_TOKEN");
 const mongoURL = Deno.env.get("MONGO_URL");
 const creatorID = Deno.env.get("CREATOR_ID");
+
 if (!token || !mongoURL) {
     console.error(envsCheckFail);
     Deno.exit(1);
@@ -44,12 +46,12 @@ export const client = new MongoClient();
 await client.connect(mongoURL);
 
 const bot = new Bot<BotContext>(token);
-const botInfo = await bot.api.getMe();
 let runner: RunnerHandle | undefined;
 
 const pm = bot.filter((ctx) => ctx.chat?.type === "private");
 const pmCreator = pm.filter((ctx) => ctx.from?.id == creatorID);
 
+bot.use(inlineQueryHandler);
 pm.use(session({
     initial() {
         return {
@@ -58,7 +60,6 @@ pm.use(session({
     },
 }));
 pm.use(favoritesMenu);
-bot.use(inlineQueryHandler);
 
 pm.use(startCommand);
 pm.use(myDataCommand);
@@ -86,21 +87,18 @@ bot.catch((err) => {
     console.error("Unknown error occurred:", error);
 });
 
-const stopOnTerm = async () => {
-    if (runner?.isRunning()) {
-        await runner.stop();
-        return true;
-    }
-    return false;
-};
+const stopRunner = () => runner?.isRunning() && runner.stop();
 
-process.once("SIGINT", stopOnTerm);
-process.once("SIGTERM", stopOnTerm);
+Deno.addSignalListener("SIGINT", stopRunner);
+Deno.addSignalListener("SIGTERM", stopRunner);
 
 try {
     runner = run(bot);
+
     await registerUserCommands(bot.api);
     await registerCreatorCommands(bot.api, creatorID);
+
+    const botInfo = await bot.api.getMe();
     console.log(`Started as ${botInfo.first_name} (@${botInfo.username})`);
 } catch (e) {
     console.log(e);
