b6eaa9f2887de849d750d079b61701f81e945767
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-01-03 09:33:33 +0300

    style(menu): reorder imports

--- src/menu/favorites.ts
+++ src/menu/favorites.ts
@@ -1,13 +1,15 @@
 import { Menu } from "@/deps.ts";
 
+import { locale } from "@/src/constants/locale.ts";
+
 import { closeMenuHandler } from "@/src/handlers/menu/closeMenuHandler.ts";
 import { dynamicListHandler } from "@/src/handlers/menu/dynamicListHandler.ts";
 import { fingerprintHandler } from "@/src/handlers/menu/fingerprintHandler.ts";
 import { nextPageHandler } from "@/src/handlers/menu/nextPageHandler.ts";
 import { outdatedHandler } from "@/src/handlers/menu/outdatedHandler.ts";
 import { prevPageHandler } from "@/src/handlers/menu/prevPageHandler.ts";
+
 import { BotContext } from "@/src/types/bot.ts";
-import { locale } from "@/src/constants/locale.ts";
 
 const { prev, close, next } = locale.frontend.favorites;
 
