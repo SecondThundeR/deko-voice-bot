e0f787ce3ab8529ef6b0b33a93024f50a8cd6bb3
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-01-14 23:20:20 +0300

    feat(database): add methods for getting stats data

---
+++ src/database/general/usersData/getUsersDataStats.ts
@@ -0,0 +1,26 @@
+import { client } from "@/bot.ts";
+
+import { collectionNames, databaseNames } from "@/src/constants/database.ts";
+
+import {
+    getCachedUsersStatsData,
+    setCachedUsersStatsData,
+} from "@/src/helpers/cache.ts";
+
+import { UsersDataSchema } from "@/src/schemas/usersData.ts";
+
+const dbName = databaseNames.general;
+const usersColName = collectionNames[dbName].usersData;
+
+export async function getUsersDataStats() {
+    const cachedUsersStats = getCachedUsersStatsData();
+    if (cachedUsersStats) return cachedUsersStats;
+
+    const db = client.database(dbName);
+    const usersData = db.collection<UsersDataSchema>(usersColName);
+
+    const usersStats = await usersData.find().toArray();
+
+    setCachedUsersStatsData(usersStats);
+    return usersStats;
+}
