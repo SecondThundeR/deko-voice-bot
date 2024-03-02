7ac22ff8e7e4e537ed279deeef7a20712b10cff4
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-03-02 13:34:43 +0300

    fix(handlers): misc change

--- src/handlers/voicesSubmenu/updateVoiceDataHandler.ts
+++ src/handlers/voicesSubmenu/updateVoiceDataHandler.ts
@@ -4,6 +4,7 @@ import { genericCloseHandler } from "@/src/handlers/menu/genericCloseHandler.ts"
 export async function updateVoiceDataHandler(ctx: MenuBotContext) {
     const conversationPath =
         ctx.session.currentVoice?.voice_file_id !== undefined ? "file" : "url";
+
     await genericCloseHandler(ctx);
     await ctx.conversation.enter(
         `voice-${conversationPath}-update`,
