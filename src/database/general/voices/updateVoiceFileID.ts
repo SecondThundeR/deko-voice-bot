03b6ff7822d72847e9be0b8651f86c8b0cfe88f3
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-03-02 13:27:15 +0300

    feat(database): add methods for updating voice URL/file ID

---
+++ src/database/general/voices/updateVoiceFileID.ts
@@ -0,0 +1,31 @@
+import { client } from "@/bot.ts";
+
+import { collectionNames, databaseNames } from "@/src/constants/database.ts";
+
+import { updateVoiceInCache } from "@/src/helpers/cache.ts";
+
+import type { VoiceSchema } from "@/src/schemas/voice.ts";
+
+const dbName = databaseNames.general;
+const colName = collectionNames[dbName].voices;
+
+export async function updateVoiceFileID(id: string, newFileID: string) {
+    const db = client.db(dbName);
+    const voices = db.collection<VoiceSchema>(colName);
+    const voiceData = await voices.findOneAndUpdate({ id }, {
+        $set: {
+            fileId: newFileID,
+        },
+    });
+
+    if (!voiceData) return false;
+    const { title, url: voice_url } = voiceData;
+    updateVoiceInCache({
+        id,
+        title,
+        type: "voice",
+        voice_file_id: newFileID,
+        voice_url,
+    });
+    return true;
+}
