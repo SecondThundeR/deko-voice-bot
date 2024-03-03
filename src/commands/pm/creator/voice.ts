535d218ecde62c5de27ad99a946eb7b7e690705c
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-03-03 12:48:21 +0300

    fix(voice): display no data message when there are no voices

--- src/commands/pm/creator/voice.ts
+++ src/commands/pm/creator/voice.ts
@@ -3,10 +3,16 @@ import { Composer } from "@/deps.ts";
 import { sendCurrentInlineRequestKeyboard } from "@/src/constants/keyboards.ts";
 
 import type { BotContext } from "@/src/types/bot.ts";
+import { checkQueriesCache } from "@/src/helpers/cache.ts";
 
 export const voiceCommand = new Composer<BotContext>();
 
 voiceCommand.command("voice", async (ctx) => {
+    const queries = checkQueriesCache();
+    if (!queries || queries.length === 0) {
+        return await ctx.reply(ctx.t("voices.noData"));
+    }
+
     await ctx.reply(ctx.t("voices.menuItemHeaderHint"), {
         reply_markup: sendCurrentInlineRequestKeyboard,
     });
