4ecff8d64aac44f4c56e8fa3b8fff1c03f29a1d7
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-08-11 13:00:04 +0300

    feat(invalidate): disable invalidation if caching is disabled

--- src/commands/pm/creator/invalidate.ts
+++ src/commands/pm/creator/invalidate.ts
@@ -7,6 +7,10 @@ import type { BotContext } from "@/src/types/bot";
 export const invalidateCommand = new Composer<BotContext>();
 
 invalidateCommand.command("invalidate", async (ctx) => {
+    if (ctx.config.isCachingDisabled) {
+        await ctx.reply(ctx.t("invalidate.disabled"));
+        return;
+    }
     invalidateVoiceCaches();
 
     await ctx.reply(ctx.t("invalidate.success"));
