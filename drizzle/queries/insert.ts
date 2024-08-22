86139b469fee8ddb442429f2732470612f3b3c0f
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-08-23 02:27:14 +0300

    feat(drizzle): add user data insert without onConflict update

--- drizzle/queries/insert.ts
+++ drizzle/queries/insert.ts
@@ -34,6 +34,25 @@ export async function addRemoteVoice(
     return insertedData.length > 0;
 }
 
+export async function insertUserData({
+    userId,
+    fullname,
+    username,
+}: Omit<InsertUser, "isIgnored" | "usesAmount" | "lastUsedAt">) {
+    const insertedData = await db
+        .insert(usersTable)
+        .values({
+            userId,
+            fullname,
+            username,
+            usesAmount: 0,
+        })
+        .onConflictDoNothing()
+        .returning();
+
+    return insertedData.length > 0;
+}
+
 export async function updateUserData({
     userId,
     fullname,
