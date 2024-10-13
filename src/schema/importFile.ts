50047ebc1294eaf2715070e8f13460f24e634304
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-10-13 23:15:50 +0300

    refactor(schema): slightly change zod parts import

--- src/schema/importFile.ts
+++ src/schema/importFile.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { object, array, string, boolean, number } from "zod";
 
 import {
     FEATURE_FLAG_NAME_LENGTH,
@@ -10,37 +10,37 @@ import {
     VOICE_TITLE_LENGTH,
 } from "@/drizzle/constraints";
 
-export const importFileSchema = z.object({
-    featureFlags: z.array(
-        z.object({
-            name: z.string().max(FEATURE_FLAG_NAME_LENGTH),
-            status: z.boolean(),
+export const importFileSchema = object({
+    featureFlags: array(
+        object({
+            name: string().max(FEATURE_FLAG_NAME_LENGTH),
+            status: boolean(),
         }),
     ),
-    voices: z.array(
-        z.object({
-            voiceId: z.string().max(VOICE_ID_LENGTH),
-            voiceTitle: z.string().max(VOICE_TITLE_LENGTH),
-            url: z.string().nullable(),
-            fileId: z.string().max(FILE_ID_LENGTH).nullable(),
-            fileUniqueId: z.string().max(FILE_UNIQUE_ID_LENGTH),
-            usesAmount: z.number().default(0),
+    voices: array(
+        object({
+            voiceId: string().max(VOICE_ID_LENGTH),
+            voiceTitle: string().max(VOICE_TITLE_LENGTH),
+            url: string().nullable(),
+            fileId: string().max(FILE_ID_LENGTH).nullable(),
+            fileUniqueId: string().max(FILE_UNIQUE_ID_LENGTH),
+            usesAmount: number().default(0),
         }),
     ),
-    users: z.array(
-        z.object({
-            userId: z.number(),
-            fullname: z.string().max(FULLNAME_LENGTH).nullable(),
-            username: z.string().max(USERNAME_LENGTH).nullable(),
-            usesAmount: z.number().default(0),
-            lastUsedAt: z.number().nullable(),
-            isIgnored: z.boolean().default(false),
+    users: array(
+        object({
+            userId: number(),
+            fullname: string().max(FULLNAME_LENGTH).nullable(),
+            username: string().max(USERNAME_LENGTH).nullable(),
+            usesAmount: number().default(0),
+            lastUsedAt: number().nullable(),
+            isIgnored: boolean().default(false),
         }),
     ),
-    usersFavorites: z.array(
-        z.object({
-            userId: z.number(),
-            voiceId: z.string().max(VOICE_ID_LENGTH),
+    usersFavorites: array(
+        object({
+            userId: number(),
+            voiceId: string().max(VOICE_ID_LENGTH),
         }),
     ),
 });
