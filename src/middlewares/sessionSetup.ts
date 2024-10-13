aacd63e8da631617ef1e4486f1c30bc6581a1144
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-10-13 23:16:39 +0300

    refactor(sessionSetup): convert functions to arrow style

--- src/middlewares/sessionSetup.ts
+++ src/middlewares/sessionSetup.ts
@@ -2,14 +2,11 @@ import { session } from "grammy";
 
 import { getSessionKey } from "@/src/helpers/api";
 
-export function sessionSetup() {
-    return session({
+export const sessionSetup = () =>
+    session({
         getSessionKey,
-        initial() {
-            return {
-                currentFavoritesOffset: 0,
-                currentVoicesOffset: 0,
-            };
-        },
+        initial: () => ({
+            currentFavoritesOffset: 0,
+            currentVoicesOffset: 0,
+        }),
     });
-}
