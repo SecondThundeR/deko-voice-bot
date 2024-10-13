9e28ca50c4ae644fef06adbf143d8cca0535ca97
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-10-13 23:38:53 +0300

    refactor(configSetup): simplify check for config setup process

--- src/middlewares/configSetup.ts
+++ src/middlewares/configSetup.ts
@@ -1,25 +1,14 @@
+import type { NextFunction } from "grammy";
+
 import type { BotContext } from "@/src/types/bot";
 
-const BASE_CONFIG = {
-    stickerFileID: process.env.STICKER_FILE_ID_FOR_DEEPLINK,
-};
-
-export function configSetup(creatorID?: string) {
-    return async (ctx: BotContext, next: () => Promise<void>) => {
-        const convertedID = Number(creatorID);
-
-        if (isNaN(convertedID)) {
-            ctx.config = {
-                ...BASE_CONFIG,
-                isCreator: false,
-            };
-        } else {
-            ctx.config = {
-                ...BASE_CONFIG,
-                creatorID: convertedID,
-                isCreator: ctx.from?.id === convertedID,
-            };
-        }
+export function configSetup(creatorID = "") {
+    return async (ctx: BotContext, next: NextFunction) => {
+        ctx.config = {
+            stickerFileID: process.env.STICKER_FILE_ID_FOR_DEEPLINK,
+            creatorID: +creatorID,
+            isCreator: ctx.from?.id === +creatorID,
+        };
 
         await next();
     };
