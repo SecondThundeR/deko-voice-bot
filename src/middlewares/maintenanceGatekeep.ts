52ae5aec34f7a0fdecc954dbb8336bd5cf96320a
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-10-13 23:41:21 +0300

    refactor(maintenanceGatekeep): slightly change and simplify logic

--- src/middlewares/maintenanceGatekeep.ts
+++ src/middlewares/maintenanceGatekeep.ts
@@ -1,42 +1,29 @@
+import type { NextFunction } from "grammy";
+
 import { getFeatureFlag } from "@/drizzle/queries/select";
 
 import { MAINTENANCE_FEATURE_FLAG } from "@/src/constants/featureFlags";
 
 import type { BotContext } from "@/src/types/bot";
 
-export async function maintenanceGatekeep(
-    ctx: BotContext,
-    next: () => Promise<void>,
-) {
-    const flagStatus = await getFeatureFlag(MAINTENANCE_FEATURE_FLAG);
-    if (!flagStatus) {
-        await next();
-        return;
-    }
+export async function maintenanceGatekeep(ctx: BotContext, next: NextFunction) {
+    const { isCreator } = ctx.config;
+    if (isCreator) return await next();
 
-    const isCalledByCreator = ctx.config.isCreator;
-    const isTriggeredByInlineQuery = ctx.inlineQuery !== undefined;
-    const isGatekeepSkipped = isCalledByCreator || !flagStatus;
+    const isInMaintenance = await getFeatureFlag(MAINTENANCE_FEATURE_FLAG);
+    if (!isInMaintenance) return await next();
 
-    if (isGatekeepSkipped) {
-        await next();
-        return;
-    }
-
-    if (isTriggeredByInlineQuery) {
-        await ctx.answerInlineQuery([], {
+    if (!!ctx.inlineQuery) {
+        return await ctx.answerInlineQuery([], {
             button: {
                 text: ctx.t("maintenance.inline-button"),
                 start_parameter: MAINTENANCE_FEATURE_FLAG,
             },
             cache_time: 0,
         });
-        return;
     }
 
     const translationPath =
-        ctx.hasCommand("start") && ctx.match === MAINTENANCE_FEATURE_FLAG
-            ? "Inline"
-            : "Chat";
+        ctx.match === MAINTENANCE_FEATURE_FLAG ? "Inline" : "Chat";
     await ctx.reply(ctx.t(`maintenance.description${translationPath}`));
 }
