0d7c2cd822e0b616a84ea0036ccd90bcf4b68160
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2024-10-19 16:25:09 +0300

    chore(package.json): bump dependencies

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,6 +1,6 @@
-import { drizzle } from "drizzle-orm/connect";
+import { drizzle } from "drizzle-orm/postgres-js";
 
-export const db = await drizzle("postgres-js", {
+export const db = drizzle({
     connection: process.env.DATABASE_URL!,
     casing: "snake_case",
     logger: process.env.NODE_ENV === "development",
