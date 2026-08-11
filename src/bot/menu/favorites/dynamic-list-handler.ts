f72567bd8c9a77c2ec23e4a10fb5cf12683ad762
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2026-08-12 02:28:52 +0300

    refactor(menu): remove redundant list pagination helper

--- src/bot/menu/favorites/dynamic-list-handler.ts
+++ src/bot/menu/favorites/dynamic-list-handler.ts
@@ -1,7 +1,6 @@
 import type { MenuRange } from "@grammyjs/menu";
 import type { Context, MenuContext } from "#root/bot/context.js";
 import { getFavoritesMenuPage } from "#root/bot/helpers/menu.js";
-import { genericListHandler } from "../generic/generic-list-handler.ts";
 import { favoriteItemHandler } from "./favorite-item-handler.ts";
 
 export async function dynamicListHandler(
@@ -15,19 +14,14 @@ export async function dynamicListHandler(
 
     const currentFavorites = await getFavoritesMenuPage(ctx, userID);
 
-    genericListHandler(range, {
-        menuElements: currentFavorites,
-        currentOffset: 0,
-        elementsPerPage: currentFavorites.length,
-        forEachCallback: (range, favoriteItem) => {
-            const { isFavored, title } = favoriteItem;
-            const isFavoredText = isFavored ? `⭐️ ` : "";
+    for (const favoriteItem of currentFavorites) {
+        const { isFavored, title } = favoriteItem;
+        const isFavoredText = isFavored ? `⭐️ ` : "";
 
-            range
-                .text(`${isFavoredText}${title}`, (ctx: MenuContext) =>
-                    favoriteItemHandler(ctx, favoriteItem),
-                )
-                .row();
-        },
-    });
+        range
+            .text(`${isFavoredText}${title}`, (ctx: MenuContext) =>
+                favoriteItemHandler(ctx, favoriteItem),
+            )
+            .row();
+    }
 }
--- src/bot/menu/voices/dynamic-list-handler.ts
+++ src/bot/menu/voices/dynamic-list-handler.ts
@@ -1,7 +1,6 @@
 import type { MenuRange } from "@grammyjs/menu";
 import type { Context } from "#root/bot/context.js";
 import { getVoicesMenuPage } from "#root/bot/helpers/menu.js";
-import { genericListHandler } from "../generic/generic-list-handler.ts";
 
 export async function dynamicListHandler(
     ctx: Context,
@@ -9,20 +8,15 @@ export async function dynamicListHandler(
 ) {
     const currentVoices = await getVoicesMenuPage(ctx);
 
-    genericListHandler(range, {
-        menuElements: currentVoices,
-        currentOffset: 0,
-        elementsPerPage: currentVoices.length,
-        forEachCallback: (range, voiceItem) => {
-            const { title } = voiceItem;
+    for (const voiceItem of currentVoices) {
+        const { title } = voiceItem;
 
-            range
-                .submenu(
-                    title,
-                    "voice-submenu",
-                    (ctx) => (ctx.session.currentVoice = voiceItem),
-                )
-                .row();
-        },
-    });
+        range
+            .submenu(
+                title,
+                "voice-submenu",
+                (ctx) => (ctx.session.currentVoice = voiceItem),
+            )
+            .row();
+    }
 }
